7aeb0291185ec5f2a65287d0c266b408548854c8
Kristian <kristianhnielsen@protonmail.com>
2025-09-21 09:19:58 +0200

    create datatable component

---
+++ components/data_table.py
@@ -0,0 +1,22 @@
+from dash import Dash, html, dash_table, dcc
+import pandas as pd
+
+
+def render(app: Dash, data: pd.DataFrame):
+    columns_for_table = ["title", "Top 3 cast", "director", "country"]
+    table = dash_table.DataTable(
+        id="data-table",
+        columns=[{"name": i, "id": i} for i in columns_for_table],
+        data=data[columns_for_table].to_dict("records"),  # type: ignore
+        page_size=10,
+        style_table={"overflowX": "auto"},
+        style_cell={
+            "textAlign": "left",
+            "backgroundColor": "#715A5A",
+            "color": "#D3DAD9",
+        },
+        style_header={"backgroundColor": "#44444E", "fontWeight": "bold"},
+        filter_action="native",
+    )
+
+    return html.Div(children=[html.H2("Data Overview"), table])
