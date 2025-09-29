0c3b562f3cbf995dddfa872c809262988efd645a
Kristian <kristianhnielsen@protonmail.com>
2025-09-29 11:04:47 +0200

    update to work with data classes and preprocessed data

--- eda.py
+++ eda.py
@@ -28,92 +28,24 @@ def _():
     import numpy as np
     import seaborn as sns
     import matplotlib.pyplot as plt
-    return mo, np, pd, plt, sns
+    return mo, plt, sns
 
 
 @app.cell
 def _():
-    return
+    from data import netflix
 
+    config = netflix.NetflixDataConfig(
+        netflix_titles_path="data/netflix_titles.csv",
+        omdb_path="data/omdb_data.csv",
+    )
+    preprocessor = netflix.NetflixDataPreprocessor()
 
-@app.cell
-def _(pd):
-    netflix_data = pd.read_csv("data/netflix_titles.csv")
+    netflix_data = netflix.NetflixData(
+        config=config, preprocessor=preprocessor
+    ).data
+    netflix_data.head()
     return (netflix_data,)
-
-
-@app.cell
-def _(netflix_data):
-    netflix_data.columns
-    return
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""## Missing Values""")
-    return
-
-
-@app.cell
-def _(netflix_data):
-    netflix_data.isnull().sum()
-    return
-
-
-@app.cell
-def _(netflix_data, np):
-    # add No Data where cast and director is NA
-    netflix_data['cast'] = netflix_data['cast'].replace(np.nan, 'No Data')
-    netflix_data['director'] = netflix_data['director'].replace(np.nan, 'No Data')
-    netflix_data.isnull().sum()
-    return
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""## Handle Datetimes""")
-    return
-
-
-@app.cell
-def _():
-    # netflix_data["date_added"] = netflix_data.to_datetime(df['date_added'])
-
-    # netflix_data['month_added'] = netflix_data['date_added'].dt.month
-    # netflix_data['month_name_added'] = netflix_data['date_added'].dt.month_name()
-    # netflix_data['year_added'] = netflix_data['date_added'].dt.year
-    return
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""## Handle multiple countries in one value""")
-    return
-
-
-@app.cell
-def _(netflix_data):
-    netflix_data[netflix_data['country'].str.contains(',', na=False)]
-    return
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""### Split the countries up into primary and secondary (where secondary is any other countries listed, if any)""")
-    return
-
-
-@app.cell
-def _(netflix_data):
-    # First, let's create a list of countries for each row by splitting the string
-    country_lists = netflix_data['country'].str.split(', ', expand=False)
-
-    # Create the 'country_primary' column by taking the first item from each list
-    netflix_data['country_primary'] = country_lists.str[0]
-
-    # Create the 'country_secondary' column by taking all other items and joining them
-    netflix_data['country_secondary'] = country_lists.str[1:].str.join(', ')
-    return
 
 
 @app.cell
@@ -131,33 +63,21 @@ def _(mo):
 
 @app.cell
 def _(netflix_data, plt, sns, top_x_countries):
-    country_val_counts = netflix_data.value_counts("country_primary")[:top_x_countries.value]
+    country_val_counts = netflix_data.value_counts("country_primary")[
+        : top_x_countries.value
+    ]
 
 
     # Plotting top X countries
     plt.figure(figsize=(5, 3))
     sns.barplot(x=country_val_counts.index, y=country_val_counts.values)
-    plt.title(f"Top {top_x_countries.value} Countries by Number of Titles on Netflix")
+    plt.title(
+        f"Top {top_x_countries.value} Countries by Number of Titles on Netflix"
+    )
     plt.xlabel("Country")
     plt.ylabel("Number of Titles")
     plt.xticks(rotation=65)
     plt.show()
-    return
-
-
-@app.cell
-def _():
-    from data import netflix
-
-    config = netflix.NetflixDataConfig(
-            netflix_titles_path="data/netflix_titles.csv", omdb_path="data/omdb_data.csv"
-        )
-    preprocessor = netflix.NetflixDataPreprocessor()
-
-    netflixData = netflix.NetflixData(config=config, preprocessor=preprocessor)
-    data = netflixData.data
-    data.head()
-
     return
 
 
